b1740451b2a25b70020c1da307cd855a90367460
spyros tomaras <stomaras@intracom-telecom.com>
2022-09-21 15:35:28 +0300

    django modelrs

--- ModelsDjango/carved_rock/store/models.py
+++ ModelsDjango/carved_rock/store/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+'''
+Migrations workflow 
 
+IMportant : Make sure your app is in INSTALLED_APS
+Step 1: CHnage model code
+Step 2: Generate migration script
+python manage.py makemigrations
+Step 3: Run migrations
+python manage.py migrate
+Id proivded automatically by django
+
+save() does SQL insert or update
+'''
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     stock_count = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(default="")
+
